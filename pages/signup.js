d4f78ffcd7fa8a64ac93f0adacd9fc67743a57bb
zaki-hanafiah <zaki@zakhov.com>
2022-09-28 14:37:32 +0800

    feat: add clear session button for register and login flows

--- pages/signup.js
+++ pages/signup.js
@@ -6,6 +6,25 @@ function getCookie(name) {
 	return match ? match[1] : null;
 }
 
+function clearCookies() {
+	const cookies = document.cookie.split(";");
+	for (let i = 0; i < cookies.length; i++)
+	{
+		const spcook = cookies[i].split("=");
+		deleteCookie(spcook[0]);
+	}
+	function deleteCookie(cookiename)
+	{
+		const d = new Date();
+		d.setDate(d.getDate() - 1);
+		const expires = ";expires=" + d;
+		const name = cookiename;
+		const value = "";
+		document.cookie = name + "=" + value + expires + "; path=/"
+	}
+	window.location = ""; // TO REFRESH THE PAGE
+}
+
 function checkIfUserRegistered() {
 	const userId = getCookie('userId');
 	const userName = getCookie('userName');
@@ -34,9 +53,10 @@ function validateForm() {
 function loadLogin() {
 	document.getElementById("btn").innerHTML = "Loading...";
 	document.getElementById("btn").classList.add('bg-green-600');
+	const form = document.getElementsByClassName("form")[0];
+	form.innerHTML = '<div class="mb-4"><p>Your key has been successfully registered.</p></div>';
 	window.setTimeout(() => {
-		let form = document.getElementsByClassName("form")[0];
-		form.innerHTML = '<div class="mb-4"><p>Your key has been successfully registered.</p><p>You are now logged in.</p></div>';
+		form.innerHTML = '<div class="mb-4"><p>Your key has been successfully registered.</p><p>You are now logged in.</p><div class="flex items-center justify-center mt-2"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onclick="clearCookies()">Clear Session</button></div></div>';
 	}, 1500);
 }
 
