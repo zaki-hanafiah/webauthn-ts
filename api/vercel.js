02bba20882183ec4f91ffcc3d2ae3e50439c2219
zaki-hanafiah <zaki@zakhov.com>
2026-02-02 03:05:47 +0800

    Fix Vercel serverless function to properly import router

--- api/vercel.js
+++ api/vercel.js
@@ -1,3 +1,22 @@
-import app from '../dist/index.js'
+const path = require('path')
+const express = require('express')
+const dotenv = require('dotenv')
 
-export default app
+dotenv.config()
+
+const app = express()
+const { router } = require(path.join(
+    process.cwd(),
+    'dist/authentication/router'
+))
+
+app.use(require('cookie-parser')())
+app.use(express.static(path.join(process.cwd(), 'pages')))
+app.use('/.well-known', express.static(path.join(process.cwd(), '.well-known')))
+app.use('/authentication', router)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'pages', 'home.html'))
+})
+
+module.exports = app
