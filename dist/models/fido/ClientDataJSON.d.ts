49049f2656ef29e84ab1b1cdc4b5cd04f0bdc204
Zaki <31648832+zaki-hanafiah@users.noreply.github.com>
2026-02-02 02:50:02 +0800

    chore: update deps with security vulnerabilities and upgrade husky hook (#22)
    * chore: remove dist output folder, fix security deps and switched to jwk native lib
    
    * chore: update rimraf to npx
    
    * chore: update node engine

--- dist/models/fido/ClientDataJSON.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ClientDataJSON {
-    challenge: string
-    origin: string
-    type: 'webauthn.create' | 'webauthn.get'
-    tokenBinding?: {
-        status: 'supported' | 'present'
-        id: string
-    }
-}
--- dist/storage/persistentKeyStore.d.ts
+++
@@ -1,3 +0,0 @@
-import { User } from '../models/custom/User'
-export declare function get(key: string): User
-export declare function set(key: string, value: User): void
